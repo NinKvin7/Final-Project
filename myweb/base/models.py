72597d6abe82c849210aeaa79dd1b0a3efe45563
nino_kvinikadze <kvinikadze.n.99@gmail.com>
2024-06-30 02:07:12 +0400

    commit

--- myweb/base/models.py
+++ myweb/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    picture = models.CharField(max_length=300)
+    name = models.CharField(max_length=50)
+    rate = models.FloatField(max_length=5)
+    year = models.IntegerField(max_length=5)
+    genre = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.name} _ {self.year}"
+
+
--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    movies = Movie.objects.all()
+    context = {"movies": movies}
+    return render(request, 'base/home.html', context)
 
 def about(request):
-    return HttpResponse(f"<h1> About Page <h1>")
+    return render(request, 'base/about.html')
